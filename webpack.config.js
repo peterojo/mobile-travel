65429ec41d12a418b0d86239dec4a3c04d6b640f
Peter Ojo <peter-ojo@hotmail.com>
2017-07-12 17:31:56 +0100

    completed desktop and mobile header styles

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+path = require("path");
+
+module.exports = {
+	entry: "./app/assets/scripts/app.js",
+	output: {
+		path: path.resolve(__dirname, "./app/temp/scripts"),
+		filename: "app.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						
+					}
+				}
+			}
+		]
+	}
+}
